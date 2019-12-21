3007b9e788bf87aa9d801087797a406e4e227d0e
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2019-12-21 14:24:31 -0800

    Seed postgres synchronous

--- database/postModel.sql
+++ database/postModel.sql
@@ -6,23 +6,23 @@ CREATE DATABASE reviewmodule;
 
 CREATE TABLE users (
     userId          SERIAL,
-    name            varchar(100),
-    image           varchar(150),
+    name            varchar(30),
+    image           varchar(90),
     PRIMARY KEY     (userId)
 );
 
 CREATE TABLE hosts (
     hostId          SERIAL,
-    hostName        varchar(100),
-    hostImage       varchar(150),
+    hostName        varchar(30),
+    hostImage       varchar(90),
     PRIMARY KEY     (hostId)
 );
 
 CREATE TABLE reviews (
     reviewId        SERIAL,
-    userId          int REFERENCES users(userId),
+    userId          int,
     date            date,
-    body            varchar(5000),
+    body            varchar(800),
     rating          smallint,
     cleanliness     smallint,
     communication   smallint,
@@ -35,8 +35,11 @@ CREATE TABLE reviews (
     amaAme          boolean,
     stySpa          boolean,
     spaCle          boolean,
-    hostId          int REFERENCES hosts(hostId),
-    hostRes         varchar(5000),
+    hostId          int,
+    hostRes         varchar(800),
     hostResDate     date,
     PRIMARY KEY     (reviewId)
 );
+
+-- REFERENCES users(userId)
+-- REFERENCES hosts(hostId)
--- database/seeding/postSeed.js
+++ database/seeding/postSeed.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promise;
 const json2csv = require('json2csv').parse;
 const faker = require('faker');
 const path = require('path');
@@ -16,7 +16,7 @@ const pool = new Pool({
 
 var hostCount = 0;
 const seedHost = function(start, end) {
-  if (hostCount === 5) {
+  if (hostCount === 20) {
     console.log('host done:', (new Date() - startTime))
     return;
   }
@@ -33,6 +33,7 @@ const seedHost = function(start, end) {
 
   const csvHosts = json2csv(hostArray, hostFields);
 
+  hostArray = [];
   
   fs.writeFileSync(path.resolve(__dirname, 'hosts.csv'), csvHosts);
   
@@ -41,97 +42,25 @@ const seedHost = function(start, end) {
 
     pool.query(`COPY hosts(hostName, hostImage) FROM '${path.resolve(__dirname, 'hosts.csv')}' DELIMITER ',';`, (err, res) => {
       if (err) console.log(err);
-      seedHost(i + 1, i + 1000000);
+      seedHost(i + 1, i + 250000);
     });
   
 };
-seedHost(1, 1000000)
+seedHost(1, 500000)
 
-hostArray = [];
 
 var userCount = 0;
 
 var seedUser = function(start, end) {
-  if (userCount === 5) {
+  if (userCount === 20) {
     console.log('user done:', (new Date() - startTime))
-    var reviewCount = 0
-    const seedReview = function(start, end) {
-      if (reviewCount === 5) {
-        console.log('user done:', (new Date() - startTime))
-        return;
-      }
-
-      reviewCount++;
-
-      const reviewsArray = [];
-
-      for (var i = start; i <= end; i += 1) {
-        const selected = [];
-        const numbReviews = Math.ceil(Math.random() * 10);
-        for (let j = 0; j <= numbReviews; j += 1) {
-          const userId = Math.ceil(Math.random() * 5000000);
-          if (selected.indexOf(userId) !== (-1)) {
-            continue;
-          } else {
-            selected.push(userId);
-          }
-          const hostRes = Math.random() >= 0.5;
-          const longRev = Math.random() <= 0.25;
-          const date = faker.date.recent(100);
-          const now = new Date();
-          const hostResDate = faker.date.between(date, now);
-          const obj = {
-            userId,
-            date,
-            body: longRev ? `${faker.lorem.paragraph()} ${faker.lorem.paragraph()}` : faker.lorem.paragraph(),
-            rating: Math.floor(Math.random() * 3 + 3),
-            cleanliness: Math.floor(Math.random() * 3 + 3),
-            communication: Math.floor(Math.random() * 3 + 3),
-            checkin: Math.floor(Math.random() * 3 + 3),
-            accuracy: Math.floor(Math.random() * 3 + 3),
-            location: Math.floor(Math.random() * 3 + 3),
-            value: Math.floor(Math.random() * 3 + 3),
-            quiRes: Math.random() >= 0.3,
-            outHos: Math.random() >= 0.3,
-            amaAme: Math.random() >= 0.3,
-            stySpa: Math.random() >= 0.3,
-            spaCle: Math.random() >= 0.3,
-            hostId: i,
-            hostRes: hostRes ? faker.lorem.paragraph() : null,
-            hostResDate: hostRes ? hostResDate : null,
-          };
-          reviewsArray.push(obj);
-        }
-      }
-      
-      const reviewFields = [
-        'userId', 'date', 'body', 'rating', 'cleanliness', 
-        'communication', 'checkin', 'accuracy', 'location', 
-        'value', 'quiRes', 'outHos', 'amaAme', 'styleSpa', 
-        'spaCle', 'hostId', 'hostRes', 'hostResDate'
-      ];
-      
-      const csvReviews = json2csv(reviewsArray, reviewFields);
-
-      console.log('Reviews Seeding: ', reviewCount);
-      
-      fs.appendFileSync(path.resolve(__dirname, 'reviews.csv'), csvReviews);
-      
-      pool.query(`COPY 
-                    reviews(userId, date, body, rating, cleanliness, communication, checkin, accuracy, 
-                      location, value, quiRes, outHos, amaAme, stySpa, spaCle, hostId, hostRes, hostResDate)
-                  FROM '${path.resolve(__dirname, 'reviews.csv')}' DELIMITER ',' CSV HEADER;`, (err, res) => {
-        if (err) console.log(err);
-        seedReview(i + 1, i + 1000000);
-      });
-    }
-    seedReview(1, 1000000);
+    seedReview(1, 200000);
     return;
   }
   userCount++;
-
+  
   let userArray = [];
-
+  
   for (var i = start; i <= end; i += 1) {
     const name = faker.name.firstName();
     const image = faker.image.avatar();
@@ -143,19 +72,112 @@ var seedUser = function(start, end) {
   
   const csvUsers = json2csv(userArray, userFields);
   
+  userArray = []
   fs.writeFileSync(path.resolve(__dirname, 'users.csv'), csvUsers);
-
+  
   console.log('User Seeding: ', userCount);
   
   pool.query(`COPY users(name, image) FROM '${path.resolve(__dirname, 'users.csv')}' DELIMITER ',' CSV HEADER;`, (err, res) => {
     if (err) console.log(err);
-    seedUser(i + 1, i + 1000000);
+    seedUser(i + 1, i + 250000);
   });
-
+  
 }
-seedUser(1, 1000000);
+seedUser(1, 250000);
 
-userArray = []
+var longParagraph = [];
+
+for (var x = 0; x < 10000; x++) {
+  longParagraph.push(faker.lorem.paragraph(sentence_count = 5));
+}
+
+var shortParagraph = [];
+
+for (var y = 0; y < 10000; y++) {
+  shortParagraph.push(faker.lorem.paragraph(sentence_count = 3));
+}
+
+var reviewCount = 0
+const seedReview = function(start, end) {
+  if (reviewCount === 25) {
+    console.log('user done:', (new Date() - startTime))
+    return;
+  }
+
+  var reviewStart = new Date();
+
+  reviewCount++;
+
+  let reviewsArray = [];
+
+  for (var i = start; i <= end; i += 1) {
+    const selected = [];
+    const numbReviews = Math.ceil(Math.random() * 10);
+    for (let j = 0; j <= numbReviews; j += 1) {
+      const userId = Math.ceil(Math.random() * 5000000);
+      if (selected.indexOf(userId) !== (-1)) {
+        continue;
+      } else {
+        selected.push(userId);
+      }
+      const hostRes = Math.random() >= 0.5;
+      const longRev = Math.random() <= 0.25;
+      const date = faker.date.recent(100);
+      const now = new Date();
+      const hostResDate = faker.date.between(date, now);
+      const obj = {
+        userId,
+        date,
+        body: longRev ? longParagraph[Math.floor(Math.random() * 10000)] : shortParagraph[Math.floor(Math.random() * 10000)],
+        rating: Math.floor(Math.random() * 3 + 3),
+        cleanliness: Math.floor(Math.random() * 3 + 3),
+        communication: Math.floor(Math.random() * 3 + 3),
+        checkin: Math.floor(Math.random() * 3 + 3),
+        accuracy: Math.floor(Math.random() * 3 + 3),
+        location: Math.floor(Math.random() * 3 + 3),
+        value: Math.floor(Math.random() * 3 + 3),
+        quiRes: Math.random() >= 0.3,
+        outHos: Math.random() >= 0.3,
+        amaAme: Math.random() >= 0.3,
+        stySpa: Math.random() >= 0.3,
+        spaCle: Math.random() >= 0.3,
+        hostId: i,
+        hostRes: hostRes ? shortParagraph[Math.floor(Math.random() * 10000)] : null,
+        hostResDate: hostRes ? hostResDate : null,
+      };
+      reviewsArray.push(obj);
+    }
+  }
+  
+  console.log('array created: ', new Date() - reviewStart)
+  const reviewFields = [
+    'userId', 'date', 'body', 'rating', 'cleanliness', 
+    'communication', 'checkin', 'accuracy', 'location', 
+    'value', 'quiRes', 'outHos', 'amaAme', 'styleSpa', 
+    'spaCle', 'hostId', 'hostRes', 'hostResDate'
+  ];
+  
+  const csvReviews = json2csv(reviewsArray, reviewFields);
+
+  console.log('Reviews Seeding: ', reviewCount);
+
+  reviewsArray = [];
+  
+  fs.writeFileSync(path.resolve(__dirname, 'reviews.csv'), csvReviews);
+
+  console.log('csv created: ', new Date() - reviewStart)
+
+  
+  pool.query(`COPY 
+                reviews(userId, date, body, rating, cleanliness, communication, checkin, accuracy, 
+                  location, value, quiRes, outHos, amaAme, stySpa, spaCle, hostId, hostRes, hostResDate)
+              FROM '${path.resolve(__dirname, 'reviews.csv')}' DELIMITER ',' CSV HEADER;`, (err, res) => {
+    if (err) console.log(err);
+    console.log('query complete: ', new Date() - reviewStart)
+
+    seedReview(i + 1, i + 200000);
+  });
+}
 
 // var reviewCount = 0
 // const seedReview = function(start, end) {
